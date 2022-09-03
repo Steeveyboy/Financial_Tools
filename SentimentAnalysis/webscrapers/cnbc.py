a6dbd96a6be68d80300f70759b324a442fe99239
Jonathon Steeves <jonathan.qsteeves@gmail.com>
2022-09-02 22:48:05 -0400

    Add: templates for more Web Scrapers

---
+++ SentimentAnalysis/webscrapers/cnbc.py
@@ -0,0 +1,15 @@
+from requests_html import HTMLSession
+
+link = "https://api.queryly.com/cnbc/json.aspx?queryly_key=31a35d40a9a64ab3&query={keyword}&endindex=0&batchsize=10&callback=&showfaceted=false&timezoneoffset=240&facetedfields=formats&facetedkey=formats%7C&facetedvalue=Articles%7C&additionalindexes=4cd6f71fbf22424d,937d600b0d0d4e23,3bfbe40caee7443e,626fdfcd96444f28"
+
+def fetch_article_headlines():
+    pass
+
+def parse_headlines():
+    pass
+
+def fetch_articles():
+    pass
+
+def parse_article():
+    pass
---
+++ SentimentAnalysis/webscrapers/twitter.py
@@ -0,0 +1,14 @@
+from requests_html import HTMLSession
+link = "https://twitter.com/i/api/2/search/adaptive.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&include_ext_has_nft_avatar=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_ext_limited_action_results=false&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_ext_collab_control=true&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&include_ext_sensitive_media_warning=true&include_ext_trusted_friends_metadata=true&send_error_codes=true&simple_quoted_tweet=true&q=%23{tesla}&count=20&query_source=typed_query&pc=1&spelling_corrections=1&include_ext_edit_control=true&ext=mediaStats%2ChighlightedLabel%2ChasNftAvatar%2CvoiceInfo%2Cenrichments%2CsuperFollowMetadata%2CunmentionInfo%2CeditControl%2Ccollab_control%2Cvibe"
+
+def fetch_article_headlines():
+    pass
+
+def parse_headlines():
+    pass
+
+def fetch_articles():
+    pass
+
+def parse_article():
+    pass
